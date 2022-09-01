78bfbc05140267dffcd914c205cfc7773e047591
faksu <dk>
2022-09-01 12:59:18 -0300

    Array en Proceso

---
+++ Array.js
@@ -0,0 +1,39 @@
+const imagenesProductos = [
+    "https://http2.mlstatic.com/D_NQ_NP_2X_619126-MLA50232132766_062022-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_2X_847906-MLA49763193559_042022-F.webp",
+    "https://http2.mlstatic.com/D_NQ_NP_991401-MLA31351342525_072019-O.webp"
+];
+
+const nombresPorductos= ["Tele", "Celu", "Compu"];
+
+/*
+let nombresProductosMio=["El archivo", "Placa de Video"];
+let imagenesProductosMio=[
+    "https://http2.mlstatic.com/D_NQ_NP_784446-MLA48152359723_112021-O.jpg",
+    "https://mexx-img-2019.s3.amazonaws.com/Placa-De-Video-GeForce-RTX-3090-24Gb-Gigabyte-Oc_43100_1.jpeg",
+];
+*/
+
+const nombresUsuario = []
+const imagenesUsuaeio = []
+
+for (let i = 0; i < imagenesProductos.length; i++) {
+    document.querySelector("#contenedor_base").innerHTML += `
+    ${nombresPorductos[i]}<p/>
+    <img width='400' height='400' src=${imagenesProductos[i]}><p/>
+    `;
+}
+
+function elementosAgregadosPorUsuario(){
+    let nombresAgregados = document.querySelector("#nombre_ingresado_usuario").value;
+    nombresUsuario.push(nombresAgregados);
+    document.querySelector("#elementos_usuario").innerHTML += `
+    ${nombresUsuario[i]}<p/>
+    `;
+}
+
+
+
+
+
+
